4938336f50c4c793589f4cf293ebf2bb16ba20d0
Gangadhar kondati <Ganga.kondati@gmail.com>
2025-05-20 15:15:20 -0700

    Added loading overlay.

--- lib/src/services/bikes/sell_your_bike.dart
+++ lib/src/services/bikes/sell_your_bike.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:poppflutter/src/services/bikes/seller_bike_details_form.dart';
-
-import '../../widgets/image_picker_selection.dart';
 
 class SellYourBike extends StatefulWidget {
   const SellYourBike({super.key});
@@ -11,8 +8,10 @@ class SellYourBike extends StatefulWidget {
   State<SellYourBike> createState() => _SellYourBikeState();
 }
 
-class _SellYourBikeState extends State<SellYourBike> {
+class _SellYourBikeState extends State<SellYourBike> with SingleTickerProviderStateMixin {
   final _formKey = GlobalKey<FormState>();
+  final ScrollController _scrollController = ScrollController();
+
   final TextEditingController sellerNameController = TextEditingController();
   final TextEditingController sellerContactController = TextEditingController();
   final TextEditingController modelNameController = TextEditingController();
@@ -22,39 +21,80 @@ class _SellYourBikeState extends State<SellYourBike> {
   String? selectedBrand;
   String? selectedState;
 
+  final double _bannerHeight = 40.0;
+  bool _isCollapsed = false;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Strictly Only 35+ HP/NM Powered Bikes",
-                style:
-                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16.0),
-              SellerBikeDetailsForm(
-                sellerNameController: sellerNameController,
-                sellerContactController: sellerContactController,
-                modelNameController: modelNameController,
-                cityController: cityController,
-                selectedBrand: selectedBrand,
-                selectedState: selectedState,
-                selectedCountryCode: selectedCountryCode,
-                onCountryCodeChanged: (val) =>
-                    setState(() => selectedCountryCode = val),
-                onBrandChanged: (val) => setState(() => selectedBrand = val),
-                onStateChanged: (val) => setState(() => selectedState = val),
-              ),
-            ],
-          ),
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      final offset = _scrollController.offset;
+      if (offset > 50 && !_isCollapsed) {
+        setState(() => _isCollapsed = true);
+      } else if (offset <= 50 && _isCollapsed) {
+        setState(() => _isCollapsed = false);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildAnimatedBanner() {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _isCollapsed ? 0 : _bannerHeight,
+      width: double.infinity,
+      color: Colors.red[400],
+      alignment: Alignment.center,
+      child: _isCollapsed
+          ? null
+          : const Text(
+        "Strictly Only 35+ HP/NM Powered Bikes",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          _buildAnimatedBanner(),
+          Expanded(
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: SellerBikeDetailsForm(
+                  sellerNameController: sellerNameController,
+                  sellerContactController: sellerContactController,
+                  modelNameController: modelNameController,
+                  cityController: cityController,
+                  selectedBrand: selectedBrand,
+                  selectedState: selectedState,
+                  selectedCountryCode: selectedCountryCode,
+                  onCountryCodeChanged: (val) =>
+                      setState(() => selectedCountryCode = val),
+                  onBrandChanged: (val) =>
+                      setState(() => selectedBrand = val),
+                  onStateChanged: (val) =>
+                      setState(() => selectedState = val),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
